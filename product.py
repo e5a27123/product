163285891d2e067b016a44eea075fff60969a1ad
Art Yeh <e5a27123@gmail.com>
2020-05-21 18:12:44 +0800

    add create_file

--- product.py
+++ product.py
@@ -13,3 +13,10 @@ print(products)
 
 for p in products:
     print(p[0], '的價格是', p[1])
+
+#寫入檔案
+with open('products.csv', 'w', encoding = 'utf-8') as f:  #打開檔案
+    f.write('商品,價格\n')
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')  #寫入檔案，寫入csv檔用,和換行做區隔
+
